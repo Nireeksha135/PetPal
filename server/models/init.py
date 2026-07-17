8d275480922e19bf5425586fa7c54b54960901c4
Nireeksha K R <nishuniree135@gmail.com>
2026-07-17 19:15:36 +0530

    Import Medicine and related classes in init.py

--- server/models/init.py
+++ server/models/init.py
@@ -1,2 +1,3 @@
 from server.models.user import User  # noqa: F401
 from server.models.pet import Pet, PetSpecies, PetGender  # noqa: F401
+from server.models.medicine import Medicine, MedicineLog, MedicineFrequency  # noqa: F401
