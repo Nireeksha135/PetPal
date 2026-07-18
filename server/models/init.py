4535258ded6796509ae7402992c94ec3ebf12176
Nireeksha K R <nishuniree135@gmail.com>
2026-07-18 20:36:09 +0530

    Add imports for Deworming and FleaTickTreatment

--- server/models/init.py
+++ server/models/init.py
@@ -2,3 +2,5 @@ from server.models.user import User  # noqa: F401
 from server.models.pet import Pet, PetSpecies, PetGender  # noqa: F401
 from server.models.medicine import Medicine, MedicineLog, MedicineFrequency  # noqa: F401
 from server.models.vaccination import Vaccination  # noqa: F401
+from server.models.deworming import Deworming  # noqa: F401
+from server.models.flea_tick import FleaTickTreatment  # noqa: F401
